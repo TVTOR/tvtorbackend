a22bfee23af567b68efec83a0a29338160fec6ba
Sunil Vishwakarma <sunilskv35@gmail.com>
2020-04-01 13:08:49 +0530

    changes in profile

--- config/staging_config.js
+++ config/staging_config.js
@@ -1,4 +1,4 @@
 module.exports = { 
     MONGODB_URI: "mongodb+srv://admin:admin123@cluster0-pvfw8.mongodb.net/canine_buds?retryWrites=true&w=majority",
-    JWT_SECRET: "EEEASDFASDFASDFASDF"
+    JWT_SECRET: "EEEASDFASDFASDFASDF",
 };
