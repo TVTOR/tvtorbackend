92cb38104af5505a9261cb9a6c005e084e167878
Sunil Vishwakarma <sunilskv35@gmail.com>
2020-04-24 16:07:39 +0530

    add procfiel

--- config/staging_config.js
+++ config/staging_config.js
@@ -1,4 +1,5 @@
 module.exports = { 
     MONGODB_URI: "mongodb+srv://ims:dMQIPxhEcDJCIysa@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
     JWT_SECRET: "EEEASDFASDFASDFASDF",
+    API_URL: "https://tutorbackend.herokuapp.com/"
 };
