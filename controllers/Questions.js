1d8b03d4985b75121512ee29996fc228abc58297
Suraj Mehta <suraj.mehta@ongraph.in>
2020-04-28 18:01:57 +0530

    question issue

--- controllers/Questions.js
+++ controllers/Questions.js
@@ -28,29 +28,44 @@ const getQuestion = async(req, res)=>{
     try {
         const detail = {};
         const dataArray = [];
-        const questionId = req.query.question;
+        let questionId = req.query.question;
          const data = await Ouestions.findOne({question_num: questionId});
          dataArray.push(data);
-         if(req.query.question == 4 && data.no_answer == 1 ){
-           const data1 =  await Ouestions.findOne({question_num: 2});
+         if(data.no_answer == 1 ){
+            questionId = parseInt(questionId)+1;
+           const data1 =  await Ouestions.findOne({question_num: questionId});
            dataArray.push(data1);
          }
-         if(req.query.question == 7 && data.no_answer == 1 ){
-            const data1 =  await Ouestions.findOne({question_num: 8});
-            dataArray.push(data1);
-          }
           detail.data = dataArray;
 
           if(data.optionTable == "location"){
              const locationdata = await Location.find({}, { location: 1, _id: 0 });
-             detail.locationdata = locationdata;
+             let locData = [];
+             const ldata = {};
+             locationdata.forEach( (loc)=>{ 
+                ldata.text = loc.location
+                ldata.value = loc.location
+                locData.push(ldata);
+             }) 
+             detail.location = locData
           }
+          
           if(data.optionTable == "subject"){
             const subjectdata = await Subject.find({}, { subject:1, _id: 0});
-            detail.subjectdata = subjectdata
+            let subDetails = [];
+            subjectdata.forEach( (sub)=>{ 
+                const subData = {};
+                console.log(sub.subject)
+                subData.text = sub.subject
+                subData.value = sub.subject
+                subDetails.push(subData);
+            }) 
+            detail.subject = subDetails;
          }
+
         return utilServices.successResponse(res, "Questions created successfully.", 200, detail);
     } catch (error) {
+        console.log(error)
         return utilServices.errorResponse(res, "Something went wrong", 400);
     }
 }
