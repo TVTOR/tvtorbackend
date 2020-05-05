fd427270a96f770791597988cfb034dd66e68aa8
Sunil Vishwakarma <sunilskv35@gmail.com>
2020-05-05 11:08:14 +0530

    remove auth

--- routers/Routes.js
+++ routers/Routes.js
@@ -53,14 +53,14 @@ router.put('/userdelete/:id', Authorization.verifyToken, User.changeUserDelete);
 router.get('/getallTManager', Authorization.verifyToken, User.getAllTManager);
 router.get('/getAllTutorsOfManager/:id', Authorization.verifyToken, User.getAllTutorsOfManager);
 
-router.post('/subject',Authorization.verifyToken,  Subject.createSubjects);
-router.get('/subject', Authorization.verifyToken, Subject.getAllSubjects);
+router.post('/subject', Subject.createSubjects);
+router.get('/subject', Subject.getAllSubjects);
 router.put('/subject/:id', Authorization.verifyToken, Subject.updateSubjects);
 router.delete('/subject/:id', Authorization.verifyToken, Subject.deleteSubjects);
 router.get('/subject/:id', Authorization.verifyToken, Subject.getSingleSubject);
 
-router.post('/location',Authorization.verifyToken, Location.createLocations);
-router.get('/location', Authorization.verifyToken, Location.getAllLocations);
+router.post('/location', Location.createLocations);
+router.get('/location', Location.getAllLocations);
 router.put('/location/:id', Authorization.verifyToken, Location.updateLocations);
 router.delete('/location/:id', Authorization.verifyToken, Location.deleteLocations);
 router.get('/location/:id', Authorization.verifyToken, Location.getSingleLocation);
