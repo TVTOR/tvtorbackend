a9d35b30f67fc44e7d3765613da23f384af2c7e4
Sunil Vishwakarma <sunilskv35@gmail.com>
2021-03-31 14:18:25 +0530

    change password

--- config/staging_config.js
+++ config/staging_config.js
@@ -1,6 +1,8 @@
 module.exports = { 
     // MONGODB_URI: "mongodb+srv://ims:RdaSdgWbPdw7K2zw@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
-    MONGODB_URI: "mongodb+srv://ims:RdaSdgWbPdw7K2zw@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
+    // MONGODB_URI: "mongodb+srv://ims:ongraph@123@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
+    // MONGODB_URI:"mongodb+srv://ims:Ongraph@123@cluster0.o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
+    MONGODB_URI:"mongodb+srv://ims:OngraphTech@cluster0.o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
     JWT_SECRET: "EEEASDFASDFASDFASDF",
     API_URL: "https://tutorbackend.herokuapp.com/",
     EMAIL: "ongraph.tvtor@gmail.com",
