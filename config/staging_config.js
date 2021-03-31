d77df823f36c75a6ff98c834b7a6ef0225fb98c0
Sunil Vishwakarma <sunilskv35@gmail.com>
2021-03-31 13:35:21 +0530

    WIP

--- config/staging_config.js
+++ config/staging_config.js
@@ -1,5 +1,6 @@
 module.exports = { 
-    MONGODB_URI: "mongodb+srv://ims:Ongraph@123@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
+    // MONGODB_URI: "mongodb+srv://ims:RdaSdgWbPdw7K2zw@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
+    MONGODB_URI: "mongodb+srv://ims:RdaSdgWbPdw7K2zw@cluster0-o2cty.mongodb.net/tutor?retryWrites=true&w=majority",
     JWT_SECRET: "EEEASDFASDFASDFASDF",
     API_URL: "https://tutorbackend.herokuapp.com/",
     EMAIL: "ongraph.tvtor@gmail.com",
